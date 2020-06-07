d240db0d9ec5ca474d57a9ba582c7909e8c56e3b
José <josecrespoguerrero@gmail.com>
2020-06-07 21:07:33 +0200

    source code

---
+++ LlvmUtils.py
@@ -0,0 +1,217 @@
+
+"""
+.. module:: llvmutils
+   :platform: Unix, Windows
+   :synopsis: Offers tools to use the llvm infrastructure 9.0.0
+
+.. moduleauthor:: Juan Carlos de la Torre <juan.detorre@uca.es>
+
+"""
+
+
+import os
+import subprocess
+import time
+from shutil import copy as copyfile
+import sys
+import numpy as np
+
+class LlvmUtils():
+    '''
+    llvmpath = llvm path
+    basepath = work path
+    bechmark = benchmark folder name, has to be in work path
+    generator = script to merge all benchmark suite
+    source = name for benchmark IR code
+    runs = how many times should the benchmark be run
+    jobid = job identifier code
+    useperf = True means use perf for runtime so perf has to be installed
+    '''
+
+    def __init__(self, llvmpath: str = "/llvm/bin/", basepath: str = "./", benchmark: str = "polybench_small",
+                 generator: str = "original_merged_generator.sh", source: str = "polybench_small_original.bc",
+                 runs: int = 1, jobid: str = "", useperf : bool = True):
+        self.llvmpath = llvmpath
+        self.basepath = basepath
+        self.benchmark = benchmark
+        self.generator = generator
+        self.source = source
+        self.runs = runs
+        self.jobid = jobid
+        self.onebyones = 0
+        self.useperf = useperf
+
+    @staticmethod
+    def get_passes() -> list:
+        all_passes = ["-tti","-tbaa","-scoped-noalias","-assumption-cache-tracker","-targetlibinfo","-verify",
+                      "-ee-instrument","-simplifycfg","-domtree","-sroa","-early-cse","-lower-expect",
+                      "-profile-summary-info","-forceattrs","-inferattrs","-callsite-splitting","-ipsccp",
+                      "-called-value-propagation","-attributor","-globalopt","-mem2reg","-deadargelim",
+                      "-basicaa","-aa","-loops","-lazy-branch-prob","-lazy-block-freq","-opt-remark-emitter",
+                      "-instcombine","-basiccg","-globals-aa","-prune-eh","-inline","-functionattrs",
+                      "-argpromotion","-memoryssa","-early-cse-memssa","-speculative-execution","-lazy-value-info",
+                      "-jump-threading","-correlated-propagation","-aggressive-instcombine","-libcalls-shrinkwrap",
+                      "-branch-prob","-block-freq","-pgo-memop-opt","-tailcallelim","-reassociate",
+                      "-loop-simplify","-lcssa-verification","-lcssa","-scalar-evolution","-loop-rotate","-licm",
+                      "-loop-unswitch","-indvars","-loop-idiom","-loop-deletion","-loop-unroll","-mldst-motion",
+                      "-phi-values","-memdep","-gvn","-memcpyopt","-sccp","-demanded-bits","-bdce","-dse",
+                      "-postdomtree","-adce","-barrier","-elim-avail-extern","-rpo-functionattrs","-globaldce",
+                      "-float2int","-loop-accesses","-loop-distribute","-loop-vectorize","-loop-load-elim",
+                      "-slp-vectorizer","-transform-warning","-alignment-from-assumptions","-strip-dead-prototypes",
+                      "-constmerge","-loop-sink","-instsimplify","-div-rem-pairs"]
+        return all_passes
+
+    # To convert the original benchmark into LLVM IR
+    def benchmark_link(self) -> None:
+        os.chdir("{}{}/".format(self.basepath,self.benchmark))
+        os.system("./{} {}".format(self.generator,self.llvmpath))
+        copyfile("{}".format(self.source),"../{}".format(self.source))
+        os.chdir("../")
+
+    # To get the runtime
+    def get_runtime(self,passes: str = "-O3") -> float:
+        if (os.path.exists("{}optimized_{}.bc".format(self.basepath,self.jobid))):
+            os.remove("{}optimized_{}.bc".format(self.basepath,self.jobid))
+        copyfile("{}{}".format(self.basepath,self.source),"{}optimized_{}.bc".format(self.basepath,self.jobid))
+        average = 0.0
+        if self.toIR(passes):
+            os.system("{}clang-10 -lm -O0 -Wno-everything -disable-llvm-optzns -disable-llvm-passes {}".format(
+                       self.llvmpath,"-Xclang -disable-O0-optnone {}optimized_{}.bc -o {}exec_{}.o".format(
+                       self.basepath,self.jobid,self.basepath,self.jobid)))
+            if self.useperf:
+                cmd = subprocess.check_output("{}runtimes.sh {}exec_{}.o {}".format(
+                          self.basepath,self.basepath,self.jobid,self.runs),shell=True)
+                runtimes = np.array(cmd.decode("utf-8")[:-1].split(","),dtype=float)
+                average = np.median(runtimes)
+            else:
+                for _ in range(self.runs):
+                    start_time = time.time()
+                    os.system("{}exec_{}.o".format(self.basepath,self.jobid))
+                    average += time.time() - start_time
+                average /= self.runs
+        else:
+            average = sys.maxsize
+        return average
+
+    # To get the number of lines of code
+    def get_codelines(self,passes: str = '-O3',source: str = "optimized.bc",
+                  output: str = "optimized.ll") -> int:
+        source = "{}{}".format(self.basepath,source.replace(".bc","_{}.bc".format(self.jobid)))
+        output = "{}{}".format(self.basepath,output.replace(".bc","_{}.bc".format(self.jobid)))
+        if self.toIR(passes):
+            self.toAssembly()
+            with open("{}optimized_{}.ll".format(self.basepath,self.jobid),'r') as file:
+                result = len(file.readlines())
+        else:
+            result = 0
+        return result
+
+    # To apply transformations
+    def toIR(self, passes: str = '-O3') -> bool:
+        if (os.path.exists("{}optimized_{}.bc".format(self.basepath,self.jobid))):
+            os.remove("{}optimized_{}.bc".format(self.basepath,self.jobid))
+        copyfile("{}{}".format(self.basepath,self.source),"{}optimized_{}.bc".format(self.basepath,self.jobid))
+        result = self.allinone(passes)
+        if not result:
+            copyfile("{}{}".format(self.basepath,self.source),"{}optimized_{}.bc".format(self.basepath,self.jobid))
+            result = self.onebyone(passes)
+        return result
+
+    # To transform from LLVM IR to assembly code
+    def toAssembly(self, source: str = "optimized.bc", output: str = "optimized.ll"):
+        source = "{}{}".format(self.basepath,source.replace(".bc","_{}.bc".format(self.jobid)))
+        output = "{}{}".format(self.basepath,output.replace(".ll","_{}.ll".format(self.jobid)))
+        os.system("{}llc-10 {}{} -o {}{}".format(self.llvmpath,
+                  self.basepath,source,self.basepath,output))
+
+    # To apply transformations in one line
+    def allinone(self, passes: str = '-O3') -> bool:
+        result = True
+        cmd = subprocess.Popen("{}opt-10 {} {}optimized_{}.bc -o {}optimized_{}.bc".format(
+                                self.llvmpath,passes,self.basepath,self.jobid,self.basepath,
+                                self.jobid),shell=True,stdout=subprocess.DEVNULL,
+                                stderr=subprocess.DEVNULL)
+        cmd.wait(timeout=20)
+        if cmd.returncode != 0:
+            cmd.kill()
+            result = False
+        return result
+    
+    # To apply transformations one by one
+    def onebyone(self, passes: str = '-O3') -> bool:
+        result = True
+        passeslist = passes.split(' ')
+        self.onebyones += 1
+        for llvm_pass in passeslist:
+            cmd = subprocess.Popen("{}opt-10 {} {}optimized_{}.bc -o {}optimized_{}.bc".format(
+                                    self.llvmpath,llvm_pass, self.basepath,self.jobid,
+                                    self.basepath,self.jobid),shell=True,
+                                    stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+            cmd.wait(timeout=10)
+            if cmd.returncode != 0:
+                cmd.kill()
+                result = False
+        return result
+        
+    # To get the number of time onebyone is run
+    def get_onebyone(self):
+        return self.onebyones
+
+    # To add a file to the output file
+    @staticmethod
+    def mergeDict(input_: str,output_: str):
+        dic = dict()
+        LlvmUtils.fileToDictionary(input_,dic)
+        LlvmUtils.fileToDictionary(output_,dic)
+        LlvmUtils.dictionaryToFile(output_,dic)
+
+    # File to dictionary
+    @staticmethod
+    def fileToDictionary(input_: str, dic: list):
+        with open(input_,"r") as file:
+                lines = file.readlines()
+                for line in lines:
+                    index = line.rfind(',')
+                    key = "["+"{}".format(line[:index])+"]"
+                    value = "{}".format(line[index+1:])[:-1]
+                    dic.update({key: value})
+
+    # Dictionary to file
+    @staticmethod
+    def dictionaryToFile(filename: str, dic: list):
+        with open(filename,"w") as file:
+                for keys,values in dic.items():
+                    key = '{}'.format(keys).replace("[","").replace("]","")
+                    key = '{}'.format(key).replace(", ",",")
+                    file.write('{},{}\n'.format(key,values))
+
+    # To encode file from passes to integers
+    @staticmethod
+    def encode(input_: str, output_: str):
+        with open(input_,'r') as inputfile:
+            lines = inputfile.readlines()
+            with open(output_,'w') as ouputfile:
+                for line in lines:
+                    index = line.rfind(',')
+                    keys = "{}".format(line[:index]).split(",")
+                    value = "{}".format(line[index+1:])[:-1]
+                    newkey = ""
+                    for key in keys:
+                        newkey += '{},'.format(LlvmUtils.get_passes().index(key))
+                    ouputfile.write('{}{}\n'.format(newkey,value))
+
+    # To decode file from integers to passes
+    @staticmethod
+    def decode(input_: str,output_: str):
+        with open(input_,'r') as inputfile:
+            lines = inputfile.readlines()
+            with open(output_,'w') as ouputfile:
+                for line in lines:
+                    index = line.rfind(',')
+                    keys = "{}".format(line[:index]).split(",")
+                    value = "{}".format(line[index+1:])[:-1]
+                    newkey = ""
+                    for key in keys:
+                        newkey += '{},'.format(LlvmUtils.get_passes()[int(key)])
+                    ouputfile.write('{}{}\n'.format(newkey,value))
+                    
---
+++ llvmMultiobjetiveProblem.py
@@ -0,0 +1,97 @@
+import sys
+
+from jmetal.core.problem import IntegerProblem
+from jmetal.core.solution import IntegerSolution
+
+from LlvmUtils import LlvmUtils
+
+class llvmMultiobjetiveProblem(IntegerProblem):
+
+    def __init__(self, is_minimization: bool = True, max_evaluations: int = 25000,
+                 from_file: bool = False, filename: str = None, solution_length: int = 100,
+                 population_size = int, offspring_population_size = int, verbose: bool = True,
+                 upper_bound : int = 86, assambly_lines: bool = True,
+                 dictionary_preloaded: bool = True, dictionary_name: str = 'llvm_dictionary.data'):
+
+        self.llvm = LlvmUtils(llvmpath='/usr/bin/', source="polybench_small/polybench_small_original.bc" ,jobid='llvm_multiobjetive', useperf=False)
+        self.number_of_variables = solution_length
+        self.lower_bound = [0 for _ in range(self.number_of_variables)]
+        self.upper_bound = [upper_bound for _ in range(self.number_of_variables)]
+        self.obj_directions = [self.MINIMIZE, self.MAXIMIZE]
+        self.obj_labels = ['runtime', 'codelines']
+        self.number_of_objectives = 2
+        self.number_of_constraints = 0
+        self.is_minimization = is_minimization
+        self.max_evaluations = max_evaluations
+        self.evaluations = 0
+        self.epoch = 1
+        self.phenotype = 0
+        self.population_size = population_size
+        self.offspring_population_size = offspring_population_size
+        self.assambly_lines = assambly_lines # False means fitness = runtime. True means fitness = code lines
+        self.dictionary = dict()
+        self.verbose = verbose
+        self.dictionary_preloaded = dictionary_preloaded
+        if self.dictionary_preloaded:
+            with open("llvm_dictionary.data","r") as file:
+                lines = file.readlines()
+                for line in lines:
+                    key = "["+"{}".format(line[:16])+"]"
+                    value = "{}".format(line[17:])[:-1]
+                    self.dictionary.update({key: value})
+
+    def get_name(self):
+        return 'Llvm Multiobjetive Problem'
+
+    def evaluate(self, solution: IntegerSolution) -> IntegerSolution:
+        self.phenotype +=1
+        limit = [self.offspring_population_size if self.epoch != 1 else self.population_size]
+        if self.phenotype%(limit[0]+1) == 0:
+            self.epoch += 1
+            self.phenotype = 1
+        key = "{}".format(solution.variables)
+        value = self.dictionary.get(key)
+        if value == None:
+            # Decoding
+            passes = ""
+            for i in range(self.number_of_variables):
+                passes += " {}".format(self.llvm.get_passes()[solution.variables[i]])
+
+            solution.objectives[0] = self.llvm.get_codelines(passes=passes)
+            solution.objectives[1] = self.llvm.get_runtime(passes=passes)
+            self.dictionary.update({key: solution.objectives})
+        else:
+            solution.objectives[0] = value[0]
+            solution.objectives[1] = value[1]
+        if self.verbose: # En este bloque hay que controlar objectives[0] y objectives[1]
+            strfitness=f"{solution.objectives}'"
+            print("evaluated solution {:3} from epoch {:3} : variables = {}, fitness = {}"\
+                   .format(self.phenotype,self.epoch,solution.variables,strfitness))
+            if self.phenotype == 1 and self.epoch == 1 :
+                with open(f"solutions_{self.population_size}_{self.offspring_population_size}.data","w") as file:
+                    file.write("{} {} {} {}\n".format("iter","epoch","variables","fitness"))
+            with open(f"solutions_{self.population_size}_{self.offspring_population_size}.data","a") as file:
+                file.write("{} {} {} {}\n"\
+                   .format(self.phenotype,self.epoch,solution.variables,strfitness))
+        return solution
+
+    def get_onebyone(self):
+        return self.llvm.get_onebyone()
+
+    ### FOR TERMINATION CRITERION ###
+    def update(self, *args, **kwargs):
+        self.evaluations = kwargs['EVALUATIONS']
+
+    ### FOR TERMINATION CRITERION ###
+    @property
+    def is_met(self):
+        met = self.evaluations >= self.max_evaluations
+        if self.phenotype*self.epoch % 100 == 0 or met:
+            filename = "new_dictionary_{}_{}_{}.data".format(self.population_size,
+                        self.offspring_population_size, self.phenotype*self.epoch)
+            with open(filename,"w") as file:
+                for keys,values in self.dictionary.items():
+                    key = '{}'.format(keys).replace("[","").replace("]","")
+                    key = '{}'.format(key).replace(", ",",")
+                    file.write('{},{}\n'.format(key,values))
+        return met
